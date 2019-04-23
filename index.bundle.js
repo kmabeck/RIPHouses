f71aa754bd66cc640acf7d5d687e395a2934724e
kmabeck <49599837+kmabeck@users.noreply.github.com>
2019-04-22 17:51:15 -0700

    },

--- index.bundle.js
+++ index.bundle.js
@@ -61,8 +61,7 @@
 	let videoElement;
 	navigator.mediaDevices.getUserMedia({
 	  audio: true,
-	  video: {facingMode: "environment"
-	};
+	  video: {facingMode: "environment"},
 	  //constraints: true;
 	  //facingMode: "environment",
 	}).then(stream => {
