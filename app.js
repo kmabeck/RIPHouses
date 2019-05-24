742ba5fac668c95bb4160b9151fee12b711e8350
kmabeck <49599837+kmabeck@users.noreply.github.com>
2019-05-23 17:24:19 -0700

    Filter play

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var constraints = { video: { facingMode: "environment" }, audio: false };
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
-   // canvas = document.querySelector("#canvas"),
+   cameraSensor = document.querySelector("#camera--sensor"),
     //cameraTrigger = document.querySelector("#camera--trigger"),
 	swipeView = document.querySelector("swipeView")
 // Access the device camera and stream to cameraView
@@ -27,18 +27,6 @@ function cameraStart() {
 	
 //};
 
-function generateScreenshot() {
-    document.getElementById("trigger").focus();
-    setTimeout(() => {
-    html2canvas(document.getElementById('capture'), {
-            logging: true,
-            profile: true,
-            useCORS: true}).then(function(canvas) {
-        var data = canvas.toDataURL('image/jpeg', 0.9);
-        var src = encodeURI(data);
-        document.getElementById('screenshot').src = src;
-        
-    }); }, 1000);
-}
+
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
